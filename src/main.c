7d0b92b282ac9bf1f3b27216eba787514344e168
RMCPDEDT024 <RMCPDEDT024@roots.com>
2025-08-30 17:23:42 +0530

    new

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@ int main() {
     printf("Starting Autonomous Cleaning Machine UI\n");
 
     // Example: get current time and print it
-    print_current_time();
+    print_curren();
 
     return 0;
 }
